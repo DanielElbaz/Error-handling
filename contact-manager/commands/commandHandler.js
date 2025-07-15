278132e88ec66ae249d7492a8f9b314a63f23351
yoavuzan <yoav547@gmail.com>
2025-07-15 14:17:41 +0300

    add implementation of addCommand searchCommand and deleteCommand

--- contact-manager/commands/commandHandler.js
+++ contact-manager/commands/commandHandler.js
@@ -1,6 +1,9 @@
 let mypath = __dirname;
 
-const Servicepath = mypath.replace("\\commands", "\\services\\contactService.js");
+const Servicepath = mypath.replace(
+  "\\commands",
+  "\\services\\contactService.js"
+);
 
 const Service = require(Servicepath);
 
@@ -11,20 +14,36 @@ function HandleAddCommand([name, email, phone]) {
   Service.addContactToData(name, email, phone);
 }
 
-function HandleDeleteCommand() {}
+function HandleDeleteCommand(email) {
+  //check if the mail is valid
+  Service.deleteContactFromData(email);
+}
 
 function HandleListCommand() {}
 
-function HandleSearchCommand() {}
+function HandleSearchCommand(name) {
+  const data = Service.searchByName(name);
+  console.log(data);
+  console.log(`=== Search Results for ${name}`);
+  if (data) {
+    data.forEach((element, index) => {
+      console.log(
+        `${index + 1} ${element.name} - ${element.email} - ${element.phone} `
+      );
+    });
+  }
+}
 
 function HandleHelpCommand() {
-console.log("Usage: node contacts.js [command] [arguments]");
-console.log("Commands:");
-console.log("  add \"name\" \"email\" \"phone\"  - Add a new contact");
-console.log("  list                        - List all contacts");
-console.log("  search \"query\"              - Search contacts by name or email");
-console.log("  delete \"email\"              - Delete contact by email");
-console.log("  help                        - Show this help message\n");
+  console.log("Usage: node contacts.js [command] [arguments]");
+  console.log("Commands:");
+  console.log('  add "name" "email" "phone"  - Add a new contact');
+  console.log("  list                        - List all contacts");
+  console.log(
+    '  search "query"              - Search contacts by name or email'
+  );
+  console.log('  delete "email"              - Delete contact by email');
+  console.log("  help                        - Show this help message\n");
 }
 
 module.exports = {
--- contact-manager/services/contactService.js
+++ contact-manager/services/contactService.js
@@ -2,14 +2,22 @@ let mypath = __dirname;
 
 const Utilspath = mypath.replace("\\services", "\\utils\\fileUtils.js");
 
-const fileUtils =  require(Utilspath);
+const fileUtils = require(Utilspath);
 
-const data = {}
+function addContactToData(sname, emails, phones) {
+  //check if there is duplicate inputs
 
-function addContactToData(name,email,phone){
-    const data = fileUtils;
-    //check if there is duplicate inputs
+  const data = fileUtils.loadFromJSON();
+  data.push({ name: sname, email: emails, phone: phones });
+  fileUtils.saveToJSON(data);
 }
 
+function deleteContactFromData(email) {
+  fileUtils.deleteFromJSON(email);
+}
 
-module.exports = {addContactToData};
+function searchByName(name) {
+  return fileUtils.findFromJSONByName(name);
+}
+
+module.exports = { addContactToData, deleteContactFromData, searchByName };
